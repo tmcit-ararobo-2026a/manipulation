e49a7bf38c34df806b9ec760c669ef0c1174b915
Kamagata0 <aroe0725@gmail.com>
2026-04-10 18:45:57 +0900

    feat:manualを追加

--- manual/src/joy_to_twist_node.cpp
+++ manual/src/joy_to_twist_node.cpp
@@ -36,7 +36,7 @@ public:
 int main(int argc, char* argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<JoyToTwistNode>());
+    rclcpp::spin(std::make_shared<manual::JoyToTwistNode>());
     rclcpp::shutdown();
     return 0;
 }
