dbbe222d71e5a68fb8edf029442ac2fc8cd77880
Kamagata0 <aroe0725@gmail.com>
2026-04-16 18:56:39 +0900

    feat:arm追加

--- manual/src/joy_to_twist_node.cpp
+++ manual/src/joy_to_twist_node.cpp
@@ -2,12 +2,14 @@
 
 #include "geometry_msgs/msg/twist.hpp"
 #include "sensor_msgs/msg/joy.hpp"
+#include "std_msgs/msg/float64.hpp"
 namespace manual {
 class JoyToTwistNode : public rclcpp::Node
 {
 private:
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr sub_joy_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr armtwist_;
 
 public:
     JoyToTwistNode() : Node("joy_to_twist_node")
@@ -15,22 +17,27 @@ public:
         sub_joy_ = this->create_subscription<sensor_msgs::msg::Joy>(
             "joy", 10, std::bind(&JoyToTwistNode::sub_joy_three, this, std::placeholders::_1)
         );
-        twist_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+        twist_    = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+        armtwist_ = this->create_publisher<std_msgs::msg::Float64>("/arm_topic", 10);
     }
     void sub_joy_three(const sensor_msgs::msg::Joy::SharedPtr msg)
     {
-        double x = msg->axes[0];  // 左側スティック（上下方向
-        double y = msg->axes[1];  // 左側スティック（左右方向）
-        double z = msg->axes[3];  // 右側スティック（左右方向）
+        double x         = msg->axes[0];  // 左側スティック（上下方向
+        double y         = msg->axes[1];  // 左側スティック（左右方向）
+        double z         = msg->axes[3];  // 右側スティック（左右方向）
+        int round_button = msg->buttons[1];
         if (std::abs(x) < 0.3) x = 0.0;
         if (std::abs(y) < 0.3) y = 0.0;
         if (std::abs(z) < 0.3) z = 0.0;
+        auto arm_data  = std_msgs::msg::Float64();
         auto send_data = geometry_msgs::msg::Twist();
         RCLCPP_INFO(this->get_logger(), "get controller : [%f,%d]", msg->axes[0], msg->buttons[1]);
+        arm_data.data       = round_button;
         send_data.linear.x  = x * 5;
         send_data.linear.y  = y * 5;
         send_data.angular.z = z * 5;
         twist_->publish(send_data);
+        armtwist_->publish(arm_data);
     }
 };
 
