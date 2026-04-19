acf0952c1ac32a9a092f4192062325fad8df47c1
kni-0830 <ayuayugonbe@gmail.com>
2026-04-19 15:33:04 +0900

    fix:名前がしっかりと変更されていなかったのでそれの反映

--- button/src/button.cpp
+++ button/src/button.cpp
@@ -1,4 +1,4 @@
-//順次他のボタンも実装していきます。
+// 順次他のボタンも実装していきます。
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 #include "std_msgs/msg/int32_multi_array.hpp"
@@ -11,7 +11,8 @@ public:
     ButtonNode() : Node("botton_for_servo")
     {
         button_sbscriver_ = this->create_subscription<sensor_msgs::msg::Joy>(
-            "joy", 10, std::bind(&ButtonNode::joy_callback, this, std::placeholders::_1));
+            "joy", 10, std::bind(&ButtonNode::joy_callback, this, std::placeholders::_1)
+        );
 
         button_publisher_ = this->create_publisher<std_msgs::msg::Int32MultiArray>("button", 10);
         RCLCPP_INFO(this->get_logger(), "Hello user! ButtonNode was started!!");
@@ -21,12 +22,18 @@ private:
     void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
     {
         auto send_data = std_msgs::msg::Int32MultiArray();
-        send_data.data.push_back(msg->buttons[0]); // cross:
-        send_data.data.push_back(msg->buttons[1]); // circle
-        send_data.data.push_back(msg->buttons[2]); // triangle
-        send_data.data.push_back(msg->buttons[3]); // square
-        RCLCPP_INFO(this->get_logger(), "get_controller_button\n cross : %d  circle : %d\n triangle : %d  square : %d ]",
-         msg->buttons[0], msg->buttons[1], msg->buttons[2], msg->buttons[3]);
+        send_data.data.push_back(msg->buttons[0]);  // cross:
+        send_data.data.push_back(msg->buttons[1]);  // circle
+        send_data.data.push_back(msg->buttons[2]);  // triangle
+        send_data.data.push_back(msg->buttons[3]);  // square
+        RCLCPP_INFO(
+            this->get_logger(),
+            "get_controller_button\n cross : %d  circle : %d\n triangle : %d  square : %d ]",
+            msg->buttons[0],
+            msg->buttons[1],
+            msg->buttons[2],
+            msg->buttons[3]
+        );
         button_publisher_->publish(send_data);
     }
 
@@ -34,7 +41,7 @@ private:
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr button_sbscriver_;
 };
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<ButtonNode>();
