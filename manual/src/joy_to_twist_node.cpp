b338bde6b213d939ecfd53ad559d751d63e7e33c
Kamagata0 <aroe0725@gmail.com>
2026-04-21 18:51:32 +0900

    feat:armdataがたされるようにした

--- manual/src/joy_to_twist_node.cpp
+++ manual/src/joy_to_twist_node.cpp
@@ -10,6 +10,7 @@ private:
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr sub_joy_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_;
     rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr armtwist_;
+    double ArmData = 0.0f;
 
 public:
     JoyToTwistNode() : Node("joy_to_twist_node")
@@ -25,13 +26,14 @@ public:
         double x = msg->axes[0];  // 左側スティック（上下方向
         double y = msg->axes[1];  // 左側スティック（左右方向）
         double z = msg->axes[3];  // 右側スティック（左右方向）
+        ArmData += msg->buttons[1] + (-1 * msg->buttons[3]);
         if (std::abs(x) < 0.3) x = 0.0;
         if (std::abs(y) < 0.3) y = 0.0;
         if (std::abs(z) < 0.3) z = 0.0;
         auto arm_data  = std_msgs::msg::Float64();
         auto send_data = geometry_msgs::msg::Twist();
         RCLCPP_INFO(this->get_logger(), "get controller : [%f,%d]", msg->axes[0], msg->buttons[1]);
-        arm_data.data       = msg->buttons[1] + (-1 * msg->buttons[3]);
+        arm_data.data       = ArmData;
         send_data.linear.x  = x * 5;
         send_data.linear.y  = y * 5;
         send_data.angular.z = z * 5;
